ddb6170124e2b1da666f3b27111d2a7244e40a32
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-20 20:15:26 -0300

    Tipos de variables

---
+++ variables.js
@@ -0,0 +1,38 @@
+var variable_;
+let variablelet_;
+
+const constante = "Hola soy una constante";
+console.log(constante);
+
+
+var a = 1;
+console.log(a);
+
+
+a = 4;
+console.log(a);
+
+
+let b = 3;
+console.log(b);
+
+
+b = 5;
+console.log(b);
+
+
+var variable = "Hola soy una variable VAR";
+
+for(var i = 0; i <3 ; i++){
+    var variable = "Soy la segunda variable";
+}
+
+console.log(variable);
+
+
+let variableLet = "Hola soy una variable LET";
+
+for(var i = 0; i<3 ; i++){
+    let variableLet = "Soy la segunda variable LET";
+}
+console.log(variableLet);
