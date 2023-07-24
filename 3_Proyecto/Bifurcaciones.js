9182d86941f579e07f2b386964e64a20f20227b9
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-24 20:29:03 -0300

    Modificaciones

--- 3_Proyecto/Bifurcaciones.js
+++ 3_Proyecto/Bifurcaciones.js
@@ -7,3 +7,15 @@ if(efectivo < saldo){
 }else{
     console.log("Saldo insuficiente");
 }
+
+let nota = 10;
+
+if(nota === 10){
+    console.log("Enhorabuena, has obtenido un sobresaliente ")
+}else if (nota ===7){
+    console.log("Buen trabajo, pero podrias haberlo echo mejor");
+}else if(nota ===1){
+    console.log("No estudiaste");
+}else{
+    console.log("Error");
+}
